18a52e217652ba408a6f171676f58886d54f32c6
JohannesIBK <mail@greuter.dev>
2022-07-30 00:02:48 +0200

    fix merge issues

--- src/plugins/Owner.py
+++ src/plugins/Owner.py
@@ -1,19 +1,23 @@
+from __future__ import annotations
+
+import os
 import traceback
 from typing import TYPE_CHECKING
 
 import discord
 from discord import app_commands
+from discord.ext import commands
 
-from lib.checks import owner_only
+from lib.extensions import Embed
+from translation import languages
 
 if TYPE_CHECKING:
     from main import Plyoox
 
 
-@owner_only()
-class OwnerCommands(app_commands.Group):
-    def __init__(self):
-        super().__init__(name="owner", description="Owner only commands for managing the bot.")
+class Owner(commands.GroupCog, group_name="owner", group_description="Owner only commands for managing the bot."):
+    def __init__(self, bot: Plyoox):
+        self.bot = bot
 
     plugin_group = app_commands.Group(name="plugin", description="Managing the Plugin system.")
 
@@ -27,7 +31,7 @@ class OwnerCommands(app_commands.Group):
         try:
             await bot.load_extension(plugin)
         except Exception:
-            embed = discord.Embed(description=f"```py\n{traceback.format_exc()}```")
+            embed = Embed(description=f"```py\n{traceback.format_exc()}```")
             await interaction.response.send_message(embed=embed)
         else:
             await interaction.response.send_message("Plugin successfully loaded.", ephemeral=True)
@@ -42,7 +46,7 @@ class OwnerCommands(app_commands.Group):
         try:
             await bot.unload_extension(plugin)
         except Exception:
-            embed = discord.Embed(description=f"```py\n{traceback.format_exc()}```")
+            embed = Embed(description=f"```py\n{traceback.format_exc()}```")
             await interaction.response.send_message(embed=embed)
         else:
             await interaction.response.send_message("Plugin successfully unloaded.", ephemeral=True)
@@ -57,7 +61,19 @@ class OwnerCommands(app_commands.Group):
         try:
             await bot.reload_extension(plugin)
         except Exception:
-            embed = discord.Embed(description=f"```py\n{traceback.format_exc()}```")
+            embed = Embed(description=f"```py\n{traceback.format_exc()}```")
             await interaction.response.send_message(embed=embed)
         else:
             await interaction.response.send_message("Plugin successfully reloaded.", ephemeral=True)
+
+    @app_commands.command(name="reload-language", description="Reloads the language files.")
+    async def reload_language(self, interaction: discord.Interaction):
+        languages._load_languages()
+
+        await interaction.response.send_message("Language files successfully reloaded.", ephemeral=True)
+
+
+async def setup(bot: Plyoox):
+    if guild_id := os.getenv("OWNER_GUILD"):
+        owner_guild = discord.Object(int(guild_id))
+        await bot.add_cog(Owner(bot), guild=owner_guild)

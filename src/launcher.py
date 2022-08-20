f2b73c9a611a8ade2ffd27585a690fa45f6fa4f6
JohannesIBK <mail@greuter.dev>
2022-08-20 20:01:30 +0200

    fix: error logger not defined

--- src/launcher.py
+++ src/launcher.py
@@ -38,6 +38,7 @@ async def generate_db():
 
     # Create database engine
     engine = async_sql.create_async_engine(os.getenv("POSTGRES").replace("postgresql://", "postgresql+asyncpg://"))
+    logger = logging.getLogger()
 
     async with engine.begin() as conn:
         logger.debug("Setup database...")

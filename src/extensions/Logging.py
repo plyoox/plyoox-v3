432f50b5581faf5435945767475bac00e1a98cc2
JohannesIBK <mail@greuter.dev>
2022-09-03 18:56:01 +0200

    fix message edit only logs bots

--- src/extensions/Logging.py
+++ src/extensions/Logging.py
@@ -176,7 +176,7 @@ class LoggingEvents(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_message_edit(self, payload: discord.RawMessageUpdateEvent):
-        if payload.data.get("author") is None or not payload.data["author"].get("bot"):
+        if payload.data.get("author") is None or payload.data["author"].get("bot"):
             return  # ignore bots due to interaction edits
 
         guild = self.bot.get_guild(payload.guild_id)

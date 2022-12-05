f13df95629b722358a8b376f2e2ac9e3e9bc8633
JohannesIBK <mail@greuter.dev>
2022-12-05 12:35:59 +0100

    remove command error handler

--- src/main.py
+++ src/main.py
@@ -88,9 +88,6 @@ class Plyoox(commands.Bot):
         logger.info("Ready")
         self.start_time = utils.utcnow()
 
-    async def on_command_error(self, context, exception) -> None:
-        pass
-
     async def _create_db_pool(self) -> None:
         try:
             self.db = await asyncpg.create_pool(os.getenv("POSTGRES_DSN"), init=database._init_db_connection)

9dcbe26937ddd8a1a28b58953d1c34ac5e10fcdd
JohannesIBK <mail@greuter.dev>
2023-05-02 21:39:22 +0200

    refactor(ci): remove unused script

--- publish.sh
+++
@@ -1,3 +0,0 @@
-docker buildx . -t plyoox/bot --platform linux/arm64
-docker tag plyoox/bot plyoox.registry.jetbrains.space/p/plyoox/plyoox/bot
-docker push plyoox.registry.jetbrains.space/p/plyoox/plyoox/bot
